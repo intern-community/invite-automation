83ff05700ea99cc81c30e39bd2ba7d1afd9071d6
greg-wolff <gregwwolff@gmail.com>
2018-04-09 20:17:11 -0700

    added invite express api

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  slackurlSF: process.env.SLACK_URL_SF || 'YOUR SLACK URL',
+  slackurlNYC: process.env.SLACK_URL_NYC || 'YOUR SLACK URL',
+  slackurlSEA: process.env.SLACK_URL_SEA || 'YOUR SLACK URL',
+  slacktokenSF: process.env.SLACK_TOKEN_SF || 'YOUR SLACK TOKEN',
+  slacktokenNYC: process.env.SLACK_TOKEN_NYC || 'YOUR SLACK TOKEN',
+  slacktokenSEA: process.env.SLACK_TOKEN_SEA || 'YOUR SLACK TOKEN',
+  airtableKey: process.env.AIRTABLE_API_KEY || 'YOUR AIRTABLE API KEY',
+  airtableBaseId: process.env.AIRTABLE_BASE_ID || 'YOUR AIRTABLE BASE ID',
+};
---
+++ index.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import config from './config';
+import request from 'request';
+import airtable from 'airtable';
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+const base = new airtable({apiKey: config.airtableKey}).base(config.airtableBaseId);
+
+app.post('/invite', function(req, res) {
+  // Post information to Airtable base
+  const city = (id) => ({
+    "sf": {
+      baseName: "SF Interns",
+      slackUrl: config.slackurlSF,
+      slackToken: config.slacktokenSF,
+    },
+    "nyc": {
+      baseName: "NYC Interns",
+      slackUrl: config.slackurlNYC,
+      slackToken: config.slacktokenNYC,
+    },
+    "sea": {
+      baseName: "SEA Interns",
+      slackUrl: config.slackurlSEA,
+      slackToken: config.slacktokenSEA,
+    }
+  })[id]
+
+  base(city(req.body.id).baseName).create({
+      "Name": req.body.name,
+      "Email": req.body.email,
+      "Company": req.body.company,
+      "School": req.body.school,
+  }, (err, record) => {
+      if (err) {
+          console.error(err);
+          return;
+      }
+  });
+
+  // Post data to the slack endpoint
+  request.post({
+      url: 'https://' + city(req.body.id).slackUrl + '/api/users.admin.invite',
+      form: {
+          email: req.body.email,
+          name: req.body.name,
+          token: city(req.body.id).slackToken,
+          set_active: true,
+      },
+  }, (err, httpResponse, body) => {
+      if (err) return res.send('error: ' + err);
+      const parsed_body = JSON.parse(body);
+      if (parsed_body.ok) {
+          res.send('ok');
+      } else {
+          let error = parsed_body.error;
+          if (error === 'already_invited' || error === 'already_in_team') {
+              res.send('existing');
+              return '';
+          } else if (error === 'invalid_email') {
+              error = 'invalid email';
+          } else if (error === 'invalid_auth') {
+              error = 'invalid authentication';
+          }
+          res.send('failed: ' + error)
+      }
+      return '';
+  });
+});
