7fe0fbb104a82a123c74b4bf2e9780db0a949dcb
greg-wolff <gregwwolff@gmail.com>
2020-04-19 22:50:33 -0700

    typo

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
   slackurlSF: process.env.SLACK_URL_SF || 'YOUR SLACK URL',
   slackurlNYC: process.env.SLACK_URL_NYC || 'YOUR SLACK URL',
   slackurlSEA: process.env.SLACK_URL_SEA || 'YOUR SLACK URL',
-  slackurlSEA: process.env.SLACK_URL_IC || 'YOUR SLACK URL',
+  slackurlIC: process.env.SLACK_URL_IC || 'YOUR SLACK URL',
   slacktokenSF: process.env.SLACK_TOKEN_SF || 'YOUR SLACK TOKEN',
   slacktokenNYC: process.env.SLACK_TOKEN_NYC || 'YOUR SLACK TOKEN',
   slacktokenSEA: process.env.SLACK_TOKEN_SEA || 'YOUR SLACK TOKEN',
-  slackurlSEA: process.env.SLACK_TOKEN_IC || 'YOUR SLACK URL',
+  slacktokenIC: process.env.SLACK_TOKEN_IC || 'YOUR SLACK URL',
   airtableKey: process.env.AIRTABLE_API_KEY || 'YOUR AIRTABLE API KEY',
   airtableBaseId: process.env.AIRTABLE_BASE_ID || 'YOUR AIRTABLE BASE ID',
 };
